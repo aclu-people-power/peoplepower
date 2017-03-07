e2eaf86d9e203e858ead5b62eca313cad6834529
Liyan David Chang <git@ldchang.com>
2017-03-07 02:20:57 -0500

    Default gulp and make actions

--- stream/gulpfile.js
+++ stream/gulpfile.js
@@ -2,14 +2,15 @@ var gulp = require('gulp');
 var csslint = require('gulp-csslint');
 var access = require('gulp-accessibility');
  
-gulp.task('css', function() {
+gulp.task('default', ['css-lint', 'accessibility-check']);
+gulp.task('css-lint', function() {
   gulp.src(['./src/css/*.css', '!./src/css/skeleton.css', '!./src/css/normalize.css'])
     .pipe(csslint({
         'adjoining-classes': false
     }))
     .pipe(csslint.formatter());
 }); 
-gulp.task('accessibility', function() {
+gulp.task('accessibility-check', function() {
   return gulp.src('./src/*.html')
     .pipe(access({
       force: true
