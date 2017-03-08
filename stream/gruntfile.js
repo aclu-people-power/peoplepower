49e93f9fe1b70ec967204691dc63676a0707986e
Liyan David Chang <git@ldchang.com>
2017-03-07 21:44:23 -0500

    html linter

--- stream/gruntfile.js
+++ stream/gruntfile.js
@@ -19,12 +19,20 @@ module.exports = function(grunt) {
         }
       },
       src: ['src/**.html']
+    },
+    htmllint: {
+      options: {
+        'indent-style': 'spaces',
+        'id-class-style': 'dash'
+      },
+      src: ['src/**.html']
     }
   });
 
   // Load plugins
   grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-accessibility');
+  grunt.loadNpmTasks('grunt-htmllint');
 
   // Default task
   grunt.registerTask('default', ['csslint', 'accessibility']);
