e388508d435d820cb8b9e6872d037ecae43dda56
Alex Gaynor <alex.gaynor@gmail.com>
2017-03-08 12:35:32 -0500

    Wired up initial action kit submission.
    This hooks it up using the code Kenneth provided, it definitely submits to ActionKit.
    
    It's not Ajax though, so navigation takes you away from the stream page. Not clear me to how to do otherwise.

--- stream/src/js/stream.js
+++ stream/src/js/stream.js
@@ -1,17 +1,4 @@
-$(document).ready(function(){
-
-        // Form submission method
-        $("#signup-form").submit(function(event) {
-                // Don't reload the page
-                event.preventDefault();
-
-                // TODO make an ajax call to load info to db
-
-                // stash original form content before replacing with a message
-                var originalFormContent = $("#join-us-form").html();
-                $("#join-us-form").html("<div class='thanks-message-div'><h3>Thanks!</h3></div>");
-
-                // Replace the form content after 5 seconds
-                setTimeout(function() { $("#join-us-form").html(originalFormContent); }, 5000);
-            });
-    });
+$(document).ready(function() {
+    actionkit.forms.contextRoot = 'https://go.peoplepower.org/context/';
+    actionkit.forms.initForm('act');
+});
