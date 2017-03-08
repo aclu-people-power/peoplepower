0ce57d08604afc19b99af8ded3937f1267d9edec
Liyan David Chang <git@ldchang.com>
2017-03-07 21:47:52 -0500

    Linters (#17)
    * Remove Gulp
    
    * CSS Lint with Grunt
    
    * Accessibility linter
    
    * html linter

---
+++ stream/gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    csslint: {
+      options: {
+        csslintrc: '.csslintrc'
+      },
+      src: ['src/css/*.css', '!src/css/normalize.css', '!src/css/skeleton.css']
+    },
+    accessibility: {
+      options: {
+        accessibilityLevel: 'WCAG2A',
+        reportLevels: {
+          notice: false,
+          warning: false,
+          error: true
+        }
+      },
+      src: ['src/**.html']
+    },
+    htmllint: {
+      options: {
+        'indent-style': 'spaces',
+        'id-class-style': 'dash'
+      },
+      src: ['src/**.html']
+    }
+  });
+
+  // Load plugins
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-accessibility');
+  grunt.loadNpmTasks('grunt-htmllint');
+
+  // Default task
+  grunt.registerTask('default', ['csslint', 'accessibility']);
+
+};
