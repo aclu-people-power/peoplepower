0b69a27ebf7cd83384bc2afaa8805b7b2c60123b
Liyan David Chang <git@ldchang.com>
2017-03-08 22:45:19 -0500

    simplify js

--- stream/src/js/stream.js
+++ stream/src/js/stream.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
-    actionkit.forms.contextRoot = 'https://go.peoplepower.org/context/';
-    actionkit.forms.initForm('act');
-});
-
-$(window).load(function() {
     $('#video').on('click', function() {
         /* TODO: Correct video URL */
         $(this).html('<div class="video-background"><iframe title="Live Stream Video" src="https://www.youtube.com/embed/e2axToBYD68?autoplay=1" allowfullscreen></iframe></div>');
     });
+
+    actionkit.forms.contextRoot = 'https://go.peoplepower.org/context/';
+    actionkit.forms.initForm('act');
 });
