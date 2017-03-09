ba49f10ff45058ff083d014228110aeda94c22e8
Liyan David Chang <git@ldchang.com>
2017-03-08 22:31:11 -0500

    Add video click to play and background image

--- stream/src/js/stream.js
+++ stream/src/js/stream.js
@@ -2,3 +2,10 @@ $(document).ready(function() {
     actionkit.forms.contextRoot = 'https://go.peoplepower.org/context/';
     actionkit.forms.initForm('act');
 });
+
+$(window).load(function() {
+    $('#video').on('click', function() {
+        /* TODO: Correct video URL */
+        $(this).html('<div class="video-background"><iframe title="Live Stream Video" src="https://www.youtube.com/embed/e2axToBYD68?autoplay=1" allowfullscreen></iframe></div>');
+    });
+});
